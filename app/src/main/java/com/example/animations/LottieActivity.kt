6dc5a4a860687346b33a1cfce0552139113009f8
Alexandr Ivanov <ai.kps42@gmail.com>
2021-10-15 15:28:49 +0700

    try states and speed for animations

--- app/src/main/java/com/example/animations/LottieActivity.kt
+++ app/src/main/java/com/example/animations/LottieActivity.kt
@@ -3,23 +3,41 @@ package com.example.animations
 import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import com.airbnb.lottie.LottieAnimationView
 
 class LottieActivity : AppCompatActivity() {
 
     lateinit var lottieView: LottieAnimationView
+    lateinit var btnPlay: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lottie)
 
         lottieView = findViewById(R.id.lavLottieView)
-        lottieView.playAnimation()
+        btnPlay = findViewById(R.id.btnPlay)
+        lottieView.progress = 0F
+        btnPlay.setOnClickListener {
+            Log.d("PROGRESS prog :", lottieView.progress.toString())
+            Log.d("PROGRESS speed :", lottieView.speed.toString())
+            if (lottieView.progress == 0.0F) {
+                lottieView.speed = 1F
+                lottieView.playAnimation()
+            } else {
+                lottieView.speed = -1F
+                lottieView.playAnimation()
+            }
+        }
+//        lottieView.playAnimation()
         lottieView.addAnimatorListener(object: Animator.AnimatorListener{
             override fun onAnimationStart(animation: Animator?) {
+                btnPlay.isEnabled = false
             }
 
             override fun onAnimationEnd(animation: Animator?) {
+                btnPlay.isEnabled = true
                 Toast.makeText(this@LottieActivity, "animation end", Toast.LENGTH_LONG).show()
             }
 
