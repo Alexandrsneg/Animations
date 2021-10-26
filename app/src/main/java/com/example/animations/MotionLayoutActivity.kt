66ac30a9e0eb9887587ebf51e319565500529bee
Alexandr Ivanov <ai.kps42@gmail.com>
2021-10-26 13:22:22 +0700

    start motionLayout transitions from code

--- app/src/main/java/com/example/animations/MotionLayoutActivity.kt
+++ app/src/main/java/com/example/animations/MotionLayoutActivity.kt
@@ -2,10 +2,44 @@ package com.example.animations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.motion.widget.MotionLayout
 
 class MotionLayoutActivity : AppCompatActivity() {
+    lateinit var mlRoot: MotionLayout
+    lateinit var btnMotion: Button
+     var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion_layout)
+
+        mlRoot = findViewById(R.id.mlRoot)
+        btnMotion = findViewById(R.id.btnMotion)
+
+        btnMotion.setOnClickListener {
+            startAnim()
+        }
+
+
+        mlRoot.addTransitionListener(object: MotionLayout.TransitionListener{
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {}
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {}
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                if (motionLayout?.currentState == R.id.finalSet) {
+                    counter++
+                    Toast.makeText(this@MotionLayoutActivity, counter.toString(), Toast.LENGTH_SHORT).show()
+                    startAnim()
+                }
+            }
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {}
+        })
+    }
+
+    private fun startAnim(){
+        if (counter < 3) {
+            mlRoot.setTransition(R.id.transition1)
+            mlRoot.transitionToEnd()
+        }
     }
 }
